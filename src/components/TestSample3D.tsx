69707c5bfa52981ef609eef163ab99561155eb06
Asif Azad <asif.azad@neo-world.com>
2023-06-14 10:29:35 +0530

    add controls to see all axis

--- src/components/TestSample3D.tsx
+++ src/components/TestSample3D.tsx
@@ -1,12 +1,22 @@
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useThree } from "@react-three/fiber";
 import Box from "./Box";
 
 function TestSample3D() {
+  const Controls = () => {
+    const {
+      camera,
+      gl: { domElement },
+    } = useThree();
+
+    return <orbitControls args={[camera, domElement]} />;
+  };
+
   return (
     <Canvas>
       <ambientLight />
       <pointLight position={[5, 5, 5]} />
-      
+      <axesHelper args={[10]} />
+      <Controls />
       {/* <pointLight position={[-51, -5, -5]} /> */}
       <Box />
     </Canvas>
