8c58dc54e46cc74f7b70b2cabd6b47cd8861be89
Asif Azad <asif.azad@neo-world.com>
2023-06-17 21:05:46 +0530

    add textures

--- src/components/TestSample3D.tsx
+++ src/components/TestSample3D.tsx
@@ -2,29 +2,41 @@ import { Canvas, useThree } from "@react-three/fiber";
 import Box from "./Box";
 import Sphere from "./Sphere";
 import { Sofa } from "../assets/3dModelsJSX/Sofa";
-import { Float, ContactShadows } from "@react-three/drei";
+import {
+  Float,
+  ContactShadows,
+  OrbitControls,
+  useHelper,
+  Plane,
+} from "@react-three/drei";
 import { RubberDuck } from "../assets/3dModelsJSX/RubberDuck";
 import TweakableBox from "./TweakableBox";
+import TextureSampleComp from "./TextureSampleComp";
 
 function TestSample3D() {
-  const Controls = () => {
-    const {
-      camera,
-      gl: { domElement },
-    } = useThree();
+  // const Controls = () => {
+  //   const {
+  //     camera,
+  //     gl: { domElement },
+  //   } = useThree();
 
-    return <orbitControls args={[camera, domElement]} />;
-  };
+  //   return <orbitControls args={[camera, domElement]} />;
+  // };
+
+  // const lightRef = useRef<PointLight>(null!);
+
+  // useHelper(lightRef, PointLightHelper, 1, "red");
 
   return (
-    <Canvas>
-      <ambientLight />
-      <pointLight position={[5, 5, 5]} intensity={3} />
-      <pointLight position={[-3, -3, 2]} />
+    <Canvas camera={{ position: [0, 40, 35] }}>
+      {/* <ambientLight />
+      <pointLight  position={[5, 5, 0]} intensity={4} />
+      <OrbitControls /> */}
+      {/* <pointLight position={[-3, -3, 2]} /> */}
       {/* <axesHelper args={[10]} /> */}
-      <Controls />
+      {/* <Controls /> */}
       {/* <pointLight position={[-51, -5, -5]} /> */}
-      {/* <Box geometry={<boxGeometry args={[1, 1, 1]} />}/> */}
+      {/* <Box geometry={<boxGeometry args={[1, 1, 1]} />} /> */}
       {/* <Sphere /> */}
       {/* <Float speed={1.4} rotationIntensity={1.5} floatIntensity={2.3}>
         <Sofa position={[0, 0.5, -1]} />
@@ -42,6 +54,7 @@ function TestSample3D() {
         // color="green"
       /> */}
       {/* <TweakableBox /> */}
+      <TextureSampleComp />
     </Canvas>
   );
 }
